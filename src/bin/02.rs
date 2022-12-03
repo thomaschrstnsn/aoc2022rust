b0cdfe69628797427a8e3ad0128d5e3dd71dfa79
Thomas Christensen <thomas@chrstnsn.dk>
2022-12-03 22:48:11 +0100

    day 2 part two

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -5,28 +5,32 @@ enum Play {
     Scissor,
 }
 
+use Play::*;
+
 use std::str::FromStr;
 #[derive(Debug)]
-enum ParsePlayError {
-    ParseErr,
+enum ParseError {
+    ParsePlay,
+    ParseWinner,
+    ParsePair,
 }
 
 impl FromStr for Play {
-    type Err = ParsePlayError;
+    type Err = ParseError;
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         match s {
-            "A" => Ok(Play::Rock),
-            "B" => Ok(Play::Paper),
-            "C" => Ok(Play::Scissor),
-            "X" => Ok(Play::Rock),
-            "Y" => Ok(Play::Paper),
-            "Z" => Ok(Play::Scissor),
-            _ => Err(ParsePlayError::ParseErr),
+            "A" => Ok(Rock),
+            "B" => Ok(Paper),
+            "C" => Ok(Scissor),
+            "X" => Ok(Rock),
+            "Y" => Ok(Paper),
+            "Z" => Ok(Scissor),
+            _ => Err(ParseError::ParsePlay),
         }
     }
 }
 
-fn map_line(line: &str) -> Option<(Play, Play)> {
+fn map_line_part1(line: &str) -> Option<(Play, Play)> {
     let spl = line.split_once(' ');
     match spl {
         Some((f, l)) => match Play::from_str(f) {
@@ -47,28 +51,42 @@ enum Winner {
     Draw,
 }
 
-fn winner((first, second): &(Play, Play)) -> Winner {
-    match (first, second) {
-        (Play::Rock, Play::Scissor) => Winner::First,
-        (Play::Rock, Play::Paper) => Winner::Second,
-        (Play::Paper, Play::Rock) => Winner::First,
-        (Play::Paper, Play::Scissor) => Winner::Second,
-        (Play::Scissor, Play::Paper) => Winner::First,
-        (Play::Scissor, Play::Rock) => Winner::Second,
-        _ => Winner::Draw,
+use Winner::*;
+
+impl FromStr for Winner {
+    type Err = ParseError;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "X" => Ok(First),
+            "Y" => Ok(Draw),
+            "Z" => Ok(Second),
+            _ => Err(ParseError::ParseWinner),
+        }
     }
 }
 
-fn score_part_one(t: &(Play, Play)) -> u32 {
-    let result_points = match winner(&t) {
-        Winner::First => 0,
-        Winner::Draw => 3,
-        Winner::Second => 6,
+fn winner((first, second): &(Play, Play)) -> Winner {
+    match (first, second) {
+        (Rock, Scissor) => First,
+        (Rock, Paper) => Second,
+        (Paper, Rock) => First,
+        (Paper, Scissor) => Second,
+        (Scissor, Paper) => First,
+        (Scissor, Rock) => Second,
+        _ => Draw,
+    }
+}
+
+fn score(t: &(Play, Play)) -> u32 {
+    let result_points = match winner(t) {
+        First => 0,
+        Draw => 3,
+        Second => 6,
     };
     let pick_points = match t.1 {
-        Play::Rock => 1,
-        Play::Paper => 2,
-        Play::Scissor => 3,
+        Rock => 1,
+        Paper => 2,
+        Scissor => 3,
     };
     pick_points + result_points
 }
@@ -76,13 +94,45 @@ fn score_part_one(t: &(Play, Play)) -> u32 {
 pub fn part_one(input: &str) -> Option<u32> {
     let plays = input
         .split('\n')
-        .map(|line| map_line(line).expect("mapping line correctly"));
-    let scores = plays.map(|t| score_part_one(&t));
+        .map(|line| map_line_part1(line).expect("mapping line correctly"));
+    let scores = plays.map(|t| score(&t));
     Some(scores.sum())
 }
 
+fn map_line_part2(line: &str) -> Result<(Play, Winner), ParseError> {
+    let spl = line.split_once(' ');
+    match spl {
+        Some((f, l)) => {
+            let f = Play::from_str(f)?;
+            let l = Winner::from_str(l)?;
+            Ok((f, l))
+        }
+        None => Err(ParseError::ParsePair),
+    }
+}
+
+fn make_plays(t: &(Play, Winner)) -> (Play, Play) {
+    match t {
+        (Rock, First) => (Rock, Scissor),
+        (Rock, Second) => (Rock, Paper),
+        (Rock, Draw) => (Rock, Rock),
+        (Scissor, First) => (Scissor, Paper),
+        (Scissor, Second) => (Scissor, Rock),
+        (Scissor, Draw) => (Scissor, Scissor),
+        (Paper, First) => (Paper, Rock),
+        (Paper, Second) => (Paper, Scissor),
+        (Paper, Draw) => (Paper, Paper),
+    }
+}
+
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let plays_with_winners = input
+        .split('\n')
+        .map(|line| map_line_part2(line).expect("mapping line correctly"));
+    let scores = plays_with_winners
+        .map(|t| make_plays(&t))
+        .map(|t| score(&t));
+    Some(scores.sum())
 }
 
 fn main() {
@@ -104,6 +154,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let input = advent_of_code::read_file("examples", 2);
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some(12));
     }
 }
