43e7337848c13a213b614305628b699d9639b854
Thomas Christensen <thomas@chrstnsn.dk>
2022-12-10 16:18:39 +0100

    day 10 part two

--- src/bin/10.rs
+++ src/bin/10.rs
@@ -32,8 +32,9 @@ fn parse_program(input: &str) -> IResult<&str, Vec<Instruction>> {
 
 pub fn part_one(input: &str) -> Option<u32> {
     let (remaining, program) = parse_program(input).expect("parses");
-    dbg!(&program);
-
+    if !remaining.is_empty() {
+        panic!("did not fully parse, remaining: {}", input);
+    }
     let mut reg_x: i32 = 1;
     let mut cycle: u32 = 1;
     let mut signal_strengths: Vec<i32> = Vec::new();
@@ -58,8 +59,62 @@ pub fn part_one(input: &str) -> Option<u32> {
     Some(signal_strengths.iter().sum::<i32>() as u32)
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+struct State {
+    reg_x: i32,
+    cycle: u32,
+    current_line: Vec<char>,
+}
+
+impl State {
+    pub fn new() -> Self {
+        State {
+            reg_x: 1,
+            cycle: 0,
+            current_line: Vec::new(),
+        }
+    }
+
+    pub fn draw_pixel(&mut self) -> Option<String> {
+        let pixel = match self.cycle as i32 - self.reg_x {
+            -1 | 0 | 1 => '#',
+            _ => '.',
+        };
+        self.current_line.push(pixel);
+
+        self.cycle += 1;
+        if self.cycle == 40 {
+            let line: String = self.current_line.iter().collect();
+            self.current_line = Vec::new();
+            self.cycle = 0;
+            return Some(line);
+        }
+        None
+    }
+}
+
+pub fn part_two(input: &str) -> Option<String> {
+    let (remaining, program) = parse_program(input).expect("parses");
+    if !remaining.is_empty() {
+        panic!("did not fully parse, remaining: {}", input);
+    }
+    let mut lines: Vec<String> = Vec::new();
+    let mut state = State::new();
+
+    for i in program {
+        let (cycle_inc, reg_x_delta) = match i {
+            Instruction::NoOp => (1, 0),
+            Instruction::AddX(d) => (2, d),
+        };
+        for _i in 0..cycle_inc {
+            let finished_line = state.draw_pixel();
+            if let Some(finished_line) = finished_line {
+                lines.push(finished_line);
+            }
+        }
+        state.reg_x += reg_x_delta;
+    }
+
+    Some(lines.join("\n"))
 }
 
 fn main() {
@@ -81,6 +136,14 @@ mod tests {
     #[test]
     fn test_part_two() {
         let input = advent_of_code::read_file("examples", 10);
-        assert_eq!(part_two(&input), None);
+        let expected: String = r#"##..##..##..##..##..##..##..##..##..##..
+###...###...###...###...###...###...###.
+####....####....####....####....####....
+#####.....#####.....#####.....#####.....
+######......######......######......####
+#######.......#######.......#######....."#
+            .to_owned();
+        dbg!(&expected.lines().collect::<Vec<&str>>());
+        assert_eq!(part_two(&input), Some(expected));
     }
 }
